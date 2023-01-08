f9a6109fc40731927aa6b26ce6802a28596b86b9
Selva Muthusamy <mselvarajumca@gmail.com>
2023-01-07 21:47:11 -0800

    Update Welcome.java

--- basic/Welcome.java
+++ basic/Welcome.java
@@ -7,7 +7,7 @@ public class Welcome {
      String names[] = {"Viki", "Selva", "Mohan", "Vignesh"} ;
      System.out.println("********************************");
      System.out.println("Welcome to java learning ::: " + name);
-     System.out.println("Welcome to java learning ::: " + names);
+     System.out.println("Welcome to java learning ::: " + names.toString());
      System.out.println("********************************");
 
   }
