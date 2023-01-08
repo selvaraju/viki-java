e2a8096765a89284fe0453323c9ed7649e181f51
Selva Muthusamy <mselvarajumca@gmail.com>
2023-01-07 21:45:31 -0800

    Update Welcome.java

--- basic/Welcome.java
+++ basic/Welcome.java
@@ -2,8 +2,10 @@ public class Welcome {
 
   public static void main (String arg[]) {
 
+     String name = "Viki";
+    
      System.out.println("********************************");
-     System.out.println("Welcome to java learning - Viki");
+     System.out.println("Welcome to java learning ::: " + name);
      System.out.println("********************************");
 
   }
