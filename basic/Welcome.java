fe9b58db091042bc15b3257ac404463e25465753
Selva Muthusamy <mselvarajumca@gmail.com>
2023-01-07 21:44:23 -0800

    Update Welcome.java

--- basic/Welcome.java
+++ basic/Welcome.java
@@ -2,7 +2,9 @@ public class Welcome {
 
   public static void main (String arg[]) {
 
+     System.out.println("********************************");
      System.out.println("Welcome to java learning - Viki");
+     System.out.println("********************************");
 
   }
 }
